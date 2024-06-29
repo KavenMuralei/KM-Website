8f2a3f717e84f7d52700dbfcec3ef552cfc9ea82
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2024-06-28 21:19:04 -0400

    Changed canvas and headers to be responsive no matter what the resolution of the device is.

--- js/saturn.js
+++ js/saturn.js
@@ -1,4 +1,7 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+
+
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1,1000);
@@ -7,23 +10,39 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#saturn'),
 });
 
+var canvas = document.getElementById('saturn');
+var heightRatio = 1.5;
+canvas.height = canvas.width * heightRatio;
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene,camera);
 
-const geometry = new THREE.TorusGeometry(10,1,16,100)
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe:true});
+const geometry = new THREE.TorusGeometry(13,1,2,100)
+const material = new THREE.MeshBasicMaterial({color: 0xFF6347});
 const torus = new THREE.Mesh(geometry,material);
+torus.translateY(-5);
 scene.add(torus)
 
-const geometry2 = new THREE.SphereGeometry( 7, 16, 8 ); 
+const geometry2 = new THREE.SphereGeometry( 10, 16, 8 ); 
 const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe:true } ); 
 const sphere = new THREE.Mesh( geometry2, material2 ); scene.add( sphere );
+sphere.translateY(-5);
 scene.add(sphere)
 
 
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+				
+  camera.aspect = window.innerWidth/ window.innerHeight;
+
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate(){
   requestAnimationFrame(animate);
@@ -35,7 +54,13 @@ function animate(){
   sphere.rotation.x+=0.01;
   sphere.rotation.y+=0.005;
   sphere.rotation.z+=0.01;
+
+  
+  
+
   renderer.render(scene,camera);
+
 }
 animate()
+
 console.log("hello")
---
+++ js/textAnimation.js
@@ -0,0 +1,21 @@
+import anime from '../node_modules/animejs/lib/anime.es.js';
+var textWrapper = document.querySelector('#nameHeader');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '#nameHeader .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  }).add({
+    targets: '#nameHeader .letter',
+    translateY: [0,-100],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+  });
